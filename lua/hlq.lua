c0cd4ef63acaf14696935f5ba7e2b9f6506161fd
phanium <91544758+phanen@users.noreply.github.com>
2025-12-30 01:51:54 +0800

    feat: dir part

--- lua/hlq.lua
+++ lua/hlq.lua
@@ -5,27 +5,32 @@ local api, fn = vim.api, vim.fn, vim.uv
 
 local ns = api.nvim_create_namespace('u.hlq')
 ---@class hlq.item: BqfQfItem
----@field id? integer
+---@field id0? integer
+---@field id1? integer
 
 ---@param buf integer
 ---@param line string
 ---@param lnum integer
 ---@param item hlq.item
 local hlitem = function(buf, line, lnum, item)
-  local path, basename = line:match('^(.-/([^/%s]+))%s-│')
-  if not path or not basename then return end
+  local path, dir, basename = line:match('^((.-/)([^/%s]+))%s-│')
+  if not path or not dir or not basename then return end
   local _, hl = require('mini.icons').get('file', path)
-  if not hl then return end
-  item.id = vim.F.npcall(
-    function()
-      return api.nvim_buf_set_extmark(buf, ns, lnum - 1, 0, {
-        id = item.id,
+  local hl_dir = 'Directory'
+  if not hl or not hl_dir then return end
+  item.id0, item.id1 = vim.F.npcall(function()
+    return api.nvim_buf_set_extmark(buf, ns, lnum - 1, 0, {
+      id = item.id0,
+      end_col = dir:len(),
+      hl_group = hl_dir,
+    }),
+      ---@diagnostic disable-next-line: redundant-return-value
+      api.nvim_buf_set_extmark(buf, ns, lnum - 1, dir:len(), {
+        id = item.id1,
         end_col = path:len(),
         hl_group = hl,
-        priority = 1000,
       })
-    end
-  )
+  end)
 end
 
 ---@param win integer
@@ -38,8 +43,7 @@ local hlq = function(win, buf)
   local items = qfs:get(win):list():items()
   for i = info.topline, info.botline do
     local v = items[i]
-    if v and v.valid == 1 and v.id then
-    elseif v and v.valid == 1 then
+    if v and v.valid == 1 then
       local line = api.nvim_buf_get_lines(buf, i - 1, i, false)[1]
       if line then hlitem(buf, line, i, v) end
     end
