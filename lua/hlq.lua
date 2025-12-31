bf2267ec73fbb0cd3b08131275f6f4c49e184d37
phanium <91544758+phanen@users.noreply.github.com>
2025-12-31 20:33:13 +0800

    feat: treesitter hl

--- lua/hlq.lua
+++ lua/hlq.lua
@@ -1,36 +1,94 @@
 ---START INJECT hlq.lua
 
 local M = {}
-local api, fn = vim.api, vim.fn, vim.uv
+local api, fn = vim.api, vim.fn
 
 local ns = api.nvim_create_namespace('u.hlq')
----@class hlq.item: BqfQfItem
----@field id0? integer
----@field id1? integer
+
+local qftf = function(info)
+  local items
+  local ret = {}
+  if info.quickfix == 1 then
+    items = fn.getqflist({ id = info.id, items = 0 }).items
+  else
+    items = fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+  end
+  local limit = 31
+  local fnameFmt1, fnameFmt2 = '%-' .. limit .. 's', '…%.' .. (limit - 1) .. 's'
+  local validFmt = '%s │%5d:%-3d│%s %s'
+  for i = info.start_idx, info.end_idx do
+    local e = items[i]
+    local fname = ''
+    local str
+    if e.valid == 1 then
+      if e.bufnr > 0 then
+        fname = fn.bufname(e.bufnr)
+        if fname == '' then
+          fname = '[No Name]'
+        else
+          fname = fname:gsub('^' .. vim.env.HOME, '~')
+        end
+        if #fname <= limit then
+          fname = fnameFmt1:format(fname)
+        else
+          fname = fnameFmt2:format(fname:sub(1 - limit))
+        end
+      end
+      local lnum = e.lnum > 99999 and -1 or e.lnum
+      local col = e.col > 999 and -1 or e.col
+      local qtype = e.type == '' and '' or ' ' .. e.type:sub(1, 1):upper()
+      str = validFmt:format(fname, lnum, col, qtype, e.text)
+    else
+      str = e.text
+    end
+    table.insert(ret, str)
+  end
+  return ret
+end
+
+local tmpbuf ---@type integer?
+local filetype_match = function(filename)
+  tmpbuf = tmpbuf and api.nvim_buf_is_valid(tmpbuf) and api.nvim_create_buf(false, true) or tmpbuf
+  return vim.filetype.match({ filename = filename, buf = tmpbuf })
+end
+
+local set_extmarks = vim.F.nil_wrap(api.nvim_buf_set_extmark)
+
+---@param title string
+---@return boolean
+local can_hl = function(title)
+  return not (
+    title:match('diagnostic')
+    or title:match('symbols')
+    or title:match('outline')
+    or title:match('^:G')
+  )
+end
 
 ---@param buf integer
 ---@param line string
----@param lnum integer
----@param item hlq.item
-local hlitem = function(buf, line, lnum, item)
-  local path, dir, basename = line:match('^((.-/)([^/%s]+))%s-│')
-  if not path or not dir or not basename then return end
+---@param lnum integer 0-index
+---@param codehl boolean
+local hlitem = function(buf, line, lnum, codehl)
+  local offset, path, dir, _, _, _, qcode =
+    line:match('^(((.-/)([^/│%s]+))%s-│%s*(%d+):(%d+)%s*│ )(.*)$')
+  if not path or not dir then return end
   local _, hl = require('mini.icons').get('file', path)
-  local hl_dir = 'Directory'
-  if not hl or not hl_dir then return end
-  item.id0, item.id1 = vim.F.npcall(function()
-    return api.nvim_buf_set_extmark(buf, ns, lnum - 1, 0, {
-      id = item.id0,
-      end_col = dir:len(),
-      hl_group = hl_dir,
-    }),
-      ---@diagnostic disable-next-line: redundant-return-value
-      api.nvim_buf_set_extmark(buf, ns, lnum - 1, dir:len(), {
-        id = item.id1,
-        end_col = path:len(),
-        hl_group = hl,
-      })
-  end)
+  if not hl then return end
+  set_extmarks(buf, ns, lnum, 0, { end_col = dir:len(), hl_group = 'Directory' })
+  set_extmarks(buf, ns, lnum, dir:len(), { end_col = path:len(), hl_group = hl })
+  if not codehl or not offset or not qcode or qcode:match('^%s+$') then return end -- TODO: diagnostic/symbols
+  local ft = filetype_match(path) -- TODO: maybe already matched in mini.icons?
+  if not ft then return end
+  local marks = require('snacks.picker.util.highlight').get_highlights({ code = qcode, ft = ft }) ---@type table
+  if not marks or not marks[1] or not marks[1][1] then return end
+  local off = offset:len()
+  for _, mark in ipairs(marks[1]) do
+    set_extmarks(buf, ns, lnum, mark.col + off, {
+      end_col = mark.end_col + off,
+      hl_group = mark.hl_group,
+    })
+  end
 end
 
 ---@param win integer
@@ -40,17 +98,22 @@ local hlq = function(win, buf)
   local qfs = require('bqf.qfwin.session')
   local info = fn.getwininfo(win)[1]
   if not info then return end
-  local items = qfs:get(win):list():items()
+  local items = vim.F.npcall(function() return qfs:get(win):list():items() end)
+  if not items then return end
+  local codehl = can_hl((vim.w[win].quickfix_title or ''):lower())
   for i = info.topline, info.botline do
     local v = items[i]
     if v and v.valid == 1 then
       local line = api.nvim_buf_get_lines(buf, i - 1, i, false)[1]
-      if line then hlitem(buf, line, i, v) end
+      if line then hlitem(buf, line, i - 1, codehl) end
     end
   end
 end
 
 function M.enable()
+  filetype_match = vim.func._memoize(1, filetype_match)
+  _G.qftf = qftf
+  vim.o.qftf = '{info -> v:lua._G.qftf(info)}'
   api.nvim_create_autocmd('BufReadPost', {
     pattern = 'quickfix',
     callback = function(ev)
